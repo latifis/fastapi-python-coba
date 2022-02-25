519516bda2d0d01e09ebaded335ca5ebbf2eca7f
Latief Irfansyah <latifirfansyah@gmail.com>
2022-02-25 10:55:05 +0700

    adding

--- src/main.py
+++ src/main.py
@@ -22,3 +22,5 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+# coba
